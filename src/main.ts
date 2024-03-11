888620008db8a976912ef83be42e69f678c3066c
Sergey Masiuk <mayerror@gmail.com>
2024-03-12 02:28:20 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import path from 'node:path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Home Library Service')
+    .setDescription('Home music library service')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // SwaggerModule.setup('api', app, document, {
+  //   yamlDocumentUrl: require('../doc/api.yaml'),
+  // });
+
   await app.listen(4000);
 }
 bootstrap();
