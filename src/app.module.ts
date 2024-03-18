62a26fd2b45ef3ebc1c576c2fc97b627f0c0df58
Sergey Masiuk <mayerror@gmail.com>
2024-03-18 18:48:19 +0300

    feat: add docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,15 +8,21 @@ import { AlbumModule } from './album/album.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'node:path';
 import { FavsModule } from './favs/favs.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+    }),
     UserModule,
     ArtistModule,
     TrackModule,
     AlbumModule,
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'doc'), // 'doc' это директория, где находится 'api.yaml'
+      rootPath: join(__dirname, '..', 'doc'),
     }),
     FavsModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import 'dotenv/config';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,7 +19,8 @@ async function bootstrap() {
     },
     explorer: true,
   });
-  const port = process.env.PORT || 3000;
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 3000;
   await app.listen(port);
 }
 bootstrap();
