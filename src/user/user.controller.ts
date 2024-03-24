bf1a95cd25d77033027f0b69c035bda717144806
Sergey Masiuk <mayerror@gmail.com>
2024-03-24 18:57:38 +0300

    fix: change service with typeorm

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,20 +7,15 @@ import {
   Delete,
   BadRequestException,
   HttpStatus,
-  NotFoundException,
   Put,
-  ForbiddenException,
   HttpCode,
   ValidationPipe,
   UsePipes,
-  ClassSerializerInterceptor,
-  UseInterceptors,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDto } from './dto/update-user.dto';
 import { validate as isValidUUID } from 'uuid';
-import { User } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
@@ -41,61 +36,32 @@ export class UserController {
   async findOne(@Param('id') id: string) {
     this.NotUuidCheck(id);
     const user = await this.userService.findOne(id);
-    this.NotFoundCheck(user, id);
     return user;
   }
 
-  // @Put(':id')
-  // @UsePipes(new ValidationPipe())
-  // async update(
-  //   @Param('id') id: string,
-  //   @Body() updatePasswordDto: UpdatePasswordDto,
-  // ) {
-  //   this.NotUuidCheck(id);
-  //   const user = await this.userService.findOne(id);
-  //   this.NotFoundCheck(user, id);
-  //   this.PasswordCheck(id, updatePasswordDto);
-  //   return await this.userService.update(id, updatePasswordDto);
-  // }
+  @Put(':id')
+  @UsePipes(new ValidationPipe())
+  async update(
+    @Param('id') id: string,
+    @Body() updatePasswordDto: UpdatePasswordDto,
+  ) {
+    this.NotUuidCheck(id);
+    return await this.userService.update(id, updatePasswordDto);
+  }
 
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    this.ExceptionsCheck(id);
-    this.userService.remove(id);
+  async remove(@Param('id') id: string) {
+    this.NotUuidCheck(id);
+    await this.userService.remove(id);
     return null;
   }
-
-  private async ExceptionsCheck(id: string) {
-    this.NotUuidCheck(id);
-    const user = await this.userService.findOne(id);
-    this.NotFoundCheck(user, id);
-    return user;
-  }
-
-  // private PasswordCheck(id: string, updatePasswordDto: UpdatePasswordDto) {
-  //   if (!this.userService.isUserPassMatch(id, updatePasswordDto)) {
-  //     throw new ForbiddenException({
-  //       statusCode: HttpStatus.FORBIDDEN,
-  //       message: `ERROR: password is wrong`,
-  //     });
-  //   }
-  // }
 
   private NotUuidCheck(id: string) {
     if (!isValidUUID(id)) {
       throw new BadRequestException({
         statusCode: HttpStatus.BAD_REQUEST,
         message: `ERROR: userId = ${id} is invalid (not uuid)`,
-      });
-    }
-  }
-
-  private NotFoundCheck(user: User | Partial<User>, id: string) {
-    if (user === undefined) {
-      throw new NotFoundException({
-        statusCode: HttpStatus.NOT_FOUND,
-        message: `ERROR: user with userId = ${id} doesn't exist`,
       });
     }
   }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
-import { v4 as uuidv4 } from 'uuid';
 import { UpdatePasswordDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -17,15 +21,17 @@ export class UserService {
     return this.usersRepository.find();
   }
 
-  findOne(id: string): Promise<User | null> {
-    return this.usersRepository.findOneBy({ id });
+  async findOne(id: string): Promise<User | null> {
+    const user = await this.userExistCheck(id);
+    return user;
   }
 
   async remove(id: string): Promise<void> {
+    await this.userExistCheck(id);
     await this.usersRepository.delete(id);
   }
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
+  create(createUserDto: CreateUserDto): Promise<User> {
     const user = this.usersRepository.create({
       login: createUserDto.login,
       password: createUserDto.password,
@@ -33,48 +39,42 @@ export class UserService {
       updatedAt: Date.now(),
     });
 
+    if (!user) {
+      throw new BadRequestException('ERROR: invalid request body');
+    }
+
     return this.usersRepository.save(user);
   }
-  // findAll() {
-  //   return this.users.map((user) => {
-  //     return this.removePassword(user);
-  //   });
-  // }
 
-  // findOne(id: string) {
-  //   const user = this.users.find((user) => user.id === id);
-  //   return this.removePassword(user);
-  // }
+  async update(id: string, updatePasswordDto: UpdatePasswordDto) {
+    await this.userExistCheck(id);
 
-  // update(id: string, updatePasswordDto: UpdatePasswordDto) {
-  //   const user = this.users.find((user) => user.id === id);
-  //   user.password = updatePasswordDto.newPassword;
-  //   user.updatedAt = Date.now();
-  //   user.version++;
-  //   return this.removePassword(user);
-  // }
-
-  // remove(id: string) {
-  //   const index = this.users.findIndex((user) => user.id === id);
-  //   this.users.splice(index, 1);
-  // }
-
-  // isUserPassMatch(id: string, updatePasswordDto: UpdatePasswordDto) {
-  //   const { oldPassword } = updatePasswordDto;
-  //   const user = this.users.find((user) => user.id === id);
-  //   return oldPassword === user.password;
-  // }
-
-  private removePassword(user?: User) {
-    if (user === undefined) {
-      return user;
+    const validPass = await this.isUserPassMatch(id, updatePasswordDto);
+    if (!validPass) {
+      throw new ForbiddenException('ERROR: invalid password');
     }
-    return {
-      id: user.id,
-      login: user.login,
-      version: user.version,
-      createdAt: user.createdAt,
-      updatedAt: user.updatedAt,
-    };
+
+    await this.usersRepository.update(id, {
+      password: updatePasswordDto.newPassword,
+      updatedAt: Date.now(),
+    });
+
+    return await this.usersRepository.findOneBy({ id });
+  }
+
+  async isUserPassMatch(id: string, updatePasswordDto: UpdatePasswordDto) {
+    const { oldPassword } = updatePasswordDto;
+    const user = await this.usersRepository.findOneBy({ id });
+    return oldPassword === user.password;
+  }
+
+  async userExistCheck(id: string) {
+    const user = await this.usersRepository.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException(
+        `ERROR: user with userId = ${id} doesn't exist`,
+      );
+    }
+    return user;
   }
 }
